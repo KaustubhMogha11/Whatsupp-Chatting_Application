a94d113071726669406e4b9236c2d13d4bb93d27
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2021-08-06 16:09:33 -0700

    Added context in order to overcome react prop drilling

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import Messenger from "./components/Messenger";
+import AccountProvider from "./context/AccountProvider";
+
 
 function App() {
   return (
     <div>
-     <Messenger/>
 
+<AccountProvider>
+
+<Messenger/>
+
+
+</AccountProvider>
+    
     </div>
   );
 }
