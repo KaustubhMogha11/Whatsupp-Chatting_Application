5017e08430f30eed685bf92fca3f8ba9fea15c5f
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2021-08-06 16:01:31 -0700

    added navbar and login dialog box

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import Messenger from "./components/Messenger";
+
+function App() {
+  return (
+    <div>
+     <Messenger/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Messenger.jsx
@@ -0,0 +1,42 @@
+
+
+import { AppBar, Toolbar, makeStyles, Box } from '@material-ui/core';
+//components
+import LoginDialog from './acoount/Login';
+
+const useStyles = makeStyles({
+    component: {
+        height: '100vh',
+        background: '#DCDCDC'
+    },
+    header: {
+        background: '#06beb6',
+        height: 115,
+        boxShadow: 'none'
+    },
+    loginHeader: {
+        background: '#00bfa5',
+        height: 200,
+        boxShadow: 'none'
+    }
+})
+const Messenger = () => {
+    const classes = useStyles();
+    // const { account } = useContext(AccountContext);
+    return (
+        <Box className={classes.component}>
+            <AppBar className={ classes.header}>
+                <Toolbar>
+
+                </Toolbar>
+            </AppBar>
+            {/* {
+                account ? <ChatDialog /> : <LoginDialog />
+                
+            } */}
+            <LoginDialog/>
+        </Box>
+    )
+}
+
+export default Messenger;
