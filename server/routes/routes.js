b76762303703f8c553a71eaed365b10253c0d4cd
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2021-08-27 02:52:49 -0700

    created conversation schema and controller

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 
-// import { newConversation, getConversation } from '../controller/conversation-controller.js';
+import { newConversation, getConversation } from '../controller/conversation-controller.js';
 import { addUser ,getUser } from '../controller/user-controller.js';
 // import { newMessage, getMessage }from '../controller/message-controller.js';
 
@@ -11,8 +11,8 @@ const route = express.Router();
 route.post('/add', addUser);
 route.get('/users', getUser);
 
-// route.post('/conversation/add', newConversation);
-// route.post('/conversation/get', getConversation);
+route.post('/conversation/add', newConversation);
+route.post('/conversation/get', getConversation);
 
 // route.post('/message/add', newMessage);
 // route.get('/message/get/:id', getMessage);
